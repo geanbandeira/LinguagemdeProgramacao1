904dd76f4ed8330a9270e1649b3300d3a4085579
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2024-10-20 15:49:06 -0300

    Update Atv1.java

--- Atv1.java
+++ Atv1.java
@@ -1,28 +1,27 @@
 package com.ifsp;
-
 import javax.swing.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class Atv1 {
-    public static void main(String[] args) {
+public class Atv1{
+	public static void main(String[] args) {
         JFrame frame = new JFrame("Ordenação de Números");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 300);
+        frame.setSize(300, 350);
         frame.setLayout(null);
-
+        
         JTextField num1Field = new JTextField();
         JTextField num2Field = new JTextField();
         JTextField num3Field = new JTextField();
         
-        JLabel label1 = new JLabel("Primeiro Número:");
-        JLabel label2 = new JLabel("Segundo Número:");
-        JLabel label3 = new JLabel("Terceiro Número:");
+        JLabel label1 = new JLabel("1° número");
+        JLabel label2 = new JLabel("2° número");
+        JLabel label3 = new JLabel("3° número");
         
-        JButton calculateButton = new JButton("Ordenar");
+        JButton calcularButton = new JButton("Ordenar");
         
-        JTextArea resultArea = new JTextArea();
-        resultArea.setEditable(false);
+        JTextArea resultadoArea = new JTextArea();
+        resultadoArea.setEditable(false);
         
         label1.setBounds(20, 20, 150, 25);
         num1Field.setBounds(160, 20, 100, 25);
@@ -33,9 +32,9 @@ public class Atv1 {
         label3.setBounds(20, 100, 150, 25);
         num3Field.setBounds(160, 100, 100, 25);
         
-        calculateButton.setBounds(20, 140, 240, 30);
+        calcularButton.setBounds(20, 140, 240, 60);
         
-        resultArea.setBounds(20, 180, 240, 60);
+        resultadoArea.setBounds(20, 220, 240, 70);
         
         frame.add(label1);
         frame.add(num1Field);
@@ -43,27 +42,27 @@ public class Atv1 {
         frame.add(num2Field);
         frame.add(label3);
         frame.add(num3Field);
-        frame.add(calculateButton);
-        frame.add(resultArea);
-
-        calculateButton.addActionListener(new ActionListener() {
+        frame.add(calcularButton);
+        frame.add(resultadoArea);
+        
+        calcularButton.addActionListener(new ActionListener(){
             @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
+            public void actionPerformed(ActionEvent e){
+                try{
                     int num1 = Integer.parseInt(num1Field.getText());
                     int num2 = Integer.parseInt(num2Field.getText());
                     int num3 = Integer.parseInt(num3Field.getText());
-
+                    
                     int[] nums = {num1, num2, num3};
                     java.util.Arrays.sort(nums);
-
-                    resultArea.setText("Números Ordenados:\n" + nums[0] + "\n" + nums[1] + "\n" + nums[2]);
-                } catch (NumberFormatException ex) {
-                    resultArea.setText("Por favor insira números válidos.");
+                    
+                    resultadoArea.setText("Números ordenados\n" + nums[0] + "\n" + nums[1] + "\n" + nums[2]);
+                } catch(NumberFormatException ex){
+                    resultadoArea.setText("Por favor insira os númerosz\n");
                 }
             }
         });
-
+        
         frame.setVisible(true);
-    }
+	}
 }
