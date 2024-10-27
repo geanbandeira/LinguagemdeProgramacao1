da18a4f76dbd7336d7f98bc041a5dd6defa7ec1a
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2024-10-27 10:38:24 -0300

    Add files via upload

---
+++ AulaEx1.java
@@ -0,0 +1,40 @@
+package com.ifsp;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AulaEx1 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Números Ímpares de 1 a 100");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setLayout(null);
+
+        JButton gerarButton = new JButton("Mostrar Números Ímpares");
+        
+        JTextArea resultadoArea = new JTextArea();
+        resultadoArea.setEditable(false);
+        
+        gerarButton.setBounds(20, 20, 250, 30);
+        resultadoArea.setBounds(20, 60, 250, 180);
+        
+        frame.add(gerarButton);
+        frame.add(resultadoArea);
+
+        gerarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                StringBuilder numerosImpares = new StringBuilder("Números Ímpares de 1 a 100:\n");
+                for (int i = 1; i <= 100; i++) {
+                    if (i % 2 != 0) {
+                        numerosImpares.append(i).append("\n");
+                    }
+                }
+                resultadoArea.setText(numerosImpares.toString());
+            }
+        });
+
+        frame.setVisible(true);
+    }
+}
