5b9b7e7e968af419540c175b8dee7fddcd418383
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2024-11-01 20:01:38 -0300

    Update aula5ex1.java

--- aula5ex1.java
+++ aula5ex1.java
@@ -5,20 +5,17 @@ public class ParesImpares {
         Random random = new Random();
         int[] numeros = new int[20];
 
-        // Gerando 20 números aleatórios entre 1 e 100
         System.out.println("Números gerados:");
         for (int i = 0; i < numeros.length; i++) {
             numeros[i] = random.nextInt(100) + 1; // Gera números entre 1 e 100
             System.out.print(numeros[i] + " ");
         }
 
-        // Arrays para armazenar pares e ímpares
         int[] pares = new int[20];
         int[] impares = new int[20];
         int contadorPares = 0;
         int contadorImpares = 0;
 
-        // Separando os números em pares e ímpares
         for (int numero : numeros) {
             if (numero % 2 == 0) {
                 pares[contadorPares] = numero;
@@ -29,13 +26,11 @@ public class ParesImpares {
             }
         }
 
-        // Exibindo os números pares
         System.out.println("\n\nNúmeros Pares:");
         for (int i = 0; i < contadorPares; i++) {
             System.out.print(pares[i] + " ");
         }
 
-        // Exibindo os números ímpares
         System.out.println("\n\nNúmeros Ímpares:");
         for (int i = 0; i < contadorImpares; i++) {
             System.out.print(impares[i] + " ");
