4af6997982e56a36c66e428dd9473540b12f68ee
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2024-11-25 22:25:05 -0300

    Update VintenumerosImparePares.java

--- Proof/src/gg/VintenumerosImparePares.java
+++ Proof/src/gg/VintenumerosImparePares.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class VintenumerosImparePares {
-
 	 public static void main(String[] args) {
 	 Scanner scanner = new Scanner(System.in);
 	 ArrayList<Integer> impares = new ArrayList<>();
