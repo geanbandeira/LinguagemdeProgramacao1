56f1c7b2b41e2fa6f576cf55ab601093fc4ece66
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2024-11-28 22:35:38 -0300

    Update UmaCincoQuinhentosNumeros.java

--- Proof/src/gg/UmaCincoQuinhentosNumeros.java
+++ Proof/src/gg/UmaCincoQuinhentosNumeros.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 public class UmaCincoQuinhentosNumeros {
 	public static void main(String[] args) {
-		 Random random = new Random();
+	Random random = new Random();
         int [] contagem = new int [5];
         for(int i = 0; i <500; i++){
             int numeroRandom = random.nextInt(5);
@@ -13,11 +13,10 @@ public class UmaCincoQuinhentosNumeros {
         System.out.println("Proporção de números entre 1 a 5:\n");
         for(int i=0; i < contagem.length; i++){
             double proporcao = (double) contagem[i] / 500;
-        System.out.printf("Número %d: %d vezes (%.2f%%)\n", (i + 1), contagem[i], proporcao *100);
-            
+        System.out.printf("Número %d: %d vezes (%.2f%%)\n", (i + 1), contagem[i], proporcao *100); 
         }
-		 }
-		}
+}
+}
 
 
 
